49c3eaec7bc39d39a5c339292bfb3d99be8e9fdc
nrdygrnma <github@nrdygrnma.tech>
2025-08-02 21:43:17 +0200

    added config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 // @ts-check
-import {
-	defineConfig,
-	passthroughImageService,
-	sharpImageService,
-} from "astro/config";
+import {defineConfig, passthroughImageService, sharpImageService,} from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+
 import mdx from "@astrojs/mdx";
-
 import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
 
 // disable image optimization in dev mode to allow the project to work on StackBlitz
@@ -16,15 +11,15 @@ const disableImageOptimization = process.env.NODE_ENV === "development";
 
 // https://astro.build/config
 export default defineConfig({
-	trailingSlash: "never",
-	site: "https://example.com",
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	integrations: [mdx(), icon(), sitemap()],
-	image: {
-		service: disableImageOptimization
-			? passthroughImageService()
-			: sharpImageService(),
-	},
+    trailingSlash: "never",
+    site: "https://example.com",
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [mdx(), icon(), sitemap()],
+    image: {
+        service: disableImageOptimization
+            ? passthroughImageService()
+            : sharpImageService(),
+    },
 });
